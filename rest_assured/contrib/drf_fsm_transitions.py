419a0b23f9d25995d467f02cb48b50a053c11ce4
ydaniv <maggotfish@gmail.com>
2014-11-12 12:14:45 +0200

    Added the from_state argument to transition()

--- rest_assured/contrib/drf_fsm_transitions.py
+++ rest_assured/contrib/drf_fsm_transitions.py
@@ -9,15 +9,19 @@ class TransitionAPITestCaseMixin(object):
     the DRF-FSM-Transition library.
     """
 
-    def transition(self, result, route, attribute='status'):
+    def transition(self, result, route, attribute='status', from_state=None):
 
         """Send request to a transition view endpoint, verify and return the response.
 
         :param result: The expected value of the instance's ``attribute``.
         :param route: The addition to the route, usually the name of the transition action's name.
         :param attribute: Name of the instance's attribute that holds the state.
+        :param from_state: A state to update the object to, to initialize the "from" state.
         :return: The view's response.
         """
+
+        if from_state is not None:
+            self.object.__class__.objects.filter(pk=self.object.pk).update(**{attribute: from_state})
 
         updateview = reverse(self.base_name + self.DETAIL_SUFFIX,
                              args=(self.object.pk,)) + '%s/' % route
