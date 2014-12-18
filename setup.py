3a33de6432cc067bb39dbe6fc94c2cf447a777f4
ydaniv <maggotfish@gmail.com>
2014-12-18 20:55:26 +0200

    Added six to deps

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email='maggotfish@gmail.com',
     license='BSD',
     packages=find_packages(),
-    install_requires=["django>=1.6", "djangorestframework>=2.4.3"],
+    install_requires=["django>=1.6", "djangorestframework>=2.4.3", "six"],
     zip_safe=False,
     cmdclass={'test': PyTest},
     classifiers=[
