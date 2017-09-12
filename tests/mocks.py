a0220c82845c92d585d1d3f025f6e4aed88f3a73
Yuri Prezument <y@yprez.com>
2017-09-12 22:37:25 +0300

    Test on Django 1.10, 1.11, DRF 3.4, 3.5, 3.6, Python 3.6 (#32)
    * Set Travis CI distribution to "precise", fix build
    
    * Test on Django==1.10, djangorestframework==3.4
    
    * Test on Django 1.11
    
    * Added an explicit fields=__all__ to fix some failing tests
    
    * Use explicit list of fields instead of __all__ since it's not backward compat
    
    * Add missing fields to serializers to fix tests
    
    * Test on DRF 3.5,3.6
    
    * Test on Python 3.6
    
    * Fix Python 3.6 build on travis

--- tests/mocks.py
+++ tests/mocks.py
@@ -76,21 +76,25 @@ class MockTestCase(BaseRESTAPITestCase):
 class StuffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stuff
+        fields = ['id', 'name', 'answer']
 
 
 class StuffHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Stuff
+        fields = ['name', 'answer', 'url']
 
 
 class RelatedStuffSerializer(serializers.ModelSerializer):
     class Meta:
         model = RelatedStuff
+        fields = ['thing']
 
 
 class RelatedStuffHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RelatedStuff
+        fields = ['thing']
 
     thing = serializers.HyperlinkedRelatedField(queryset=Stuff.objects.all(),
                                                 view_name='stuff-linked-detail')
@@ -99,11 +103,13 @@ class RelatedStuffHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
 class ManyRelatedStuffSerializer(serializers.ModelSerializer):
     class Meta:
         model = ManyRelatedStuff
+        fields = ['stuff']
 
 
 class ManyRelatedStuffHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ManyRelatedStuff
+        fields = ['stuff']
 
     # stuff = serializers.HyperlinkedRelatedField(queryset=Stuff.objects.all(),
     #                                             many=True)
