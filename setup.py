34b53df494ef9df2733533f327e7d424b20eb089
Yehonatan Daniv <yehonatand@wix.com>
2020-07-31 21:10:25 +0300

    Fixed long description on pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+from os import path
 from setuptools import setup, find_packages
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-rest-assured',
-    version='0.2.2',
+    version='0.2.3',
     description='Django REST Assured instantly test-covers your Django REST Framework based API.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     url='https://github.com/ydaniv/django-rest-assured',
     author='Yehonatan Daniv',
     author_email='maggotfish@gmail.com',
